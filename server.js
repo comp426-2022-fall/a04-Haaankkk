940c558478acebfcb11dae1d7524a99d4d5ae1d3
Haaankkk <hepfhank@sina.com>
2022-11-29 10:14:35 -0500

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const args = minimist(process.argv.slice(2));
 const app = express();
 const port = args.port || 5000;
 
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('*', (req, res) => {
